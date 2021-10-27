c65cb303d8229aa38047c77a0344f9d06f976fa9
Maxim Kolesnikov <makskrutoy_2015@mail.ru>
2021-10-27 18:39:15 +0300

    Lesson_1 added.

---
+++ Course_5 Lesson_1/Course_5 Lesson_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Здравствуйте вас приветствует математическая программа \nПожалуйста введите число: \nДля выхода нажмите Q");
+
+        String S = Console.ReadLine();
+
+        if (S == "q" || S == "Q")
+        {
+            Console.WriteLine("До свидания");
+            return;
+        }
+        int M = Int32.Parse(S);
+        int c1 = 1; int c2 = 0;
+        int c3 = 0;
+        for (int i = 1; i <= M; i++)
+        {
+            c1 = c1 * i;
+            c2 = c2 + i;
+            if (i % 2 == 0)
+            {
+                c3 = i;
+            }
+        }
+
+        Console.WriteLine($"Факториал равет {c1} \nСумма цифр от 1 до {S} равна {c2} \nМаксимальное четное число меньше N равно {c3} " );
+        Console.ReadLine();
+    }
+}
