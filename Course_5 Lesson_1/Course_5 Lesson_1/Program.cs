bb1d970834cdcc05e7353a633c9188824dcb8f27
Maxim Kolesnikov <makskrutoy_2015@mail.ru>
2021-10-27 22:39:13 +0300

    Lesson_1.2 added.

--- Course_5 Lesson_1/Course_5 Lesson_1/Program.cs
+++ Course_5 Lesson_1/Course_5 Lesson_1/Program.cs
@@ -2,31 +2,47 @@
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Здравствуйте вас приветствует математическая программа \nПожалуйста введите число: \nДля выхода нажмите Q");
+        Console.WriteLine("Здравствуйте вас приветствует математическая программа \nПожалуйста введите целое,положительное число: \nДля выхода нажмите Q");
 
         String S = Console.ReadLine();
 
-        if (S == "q" || S == "Q")
+        if(S == "q" || S == "Q")
         {
             Console.WriteLine("До свидания");
             return;
         }
-        int M = Int32.Parse(S);
-        int c1 = 1; int c2 = 0;
+
+        int M;
+        int c1 = 1;
+        int c2 = 0;
         int c3 = 0;
-        for (int i = 1; i <= M; i++)
+
+        bool success = int.TryParse(S ,out M);
+
+        if (success)
         {
-            c1 = c1 * i;
-            c2 = c2 + i;
-            if (i % 2 == 0)
+            for (int i = 1; i <= M; i++)
             {
-                c3 = i;
+                c1 = c1 * i;
+                c2 = c2 + i;
+                if (i % 2 == 0)
+                {
+                    if (i - 2 % 2 != 0)
+                    {
+                        c3 = i - 2;
+                    }
+                }
             }
         }
+        else
+        {
+            Console.WriteLine("До свидания");
+            return;
+        }
 
-        Console.WriteLine($"Факториал равет {c1} \nСумма цифр от 1 до {S} равна {c2} \nМаксимальное четное число меньше N равно {c3} " );
+        Console.WriteLine($"Факториал равен {c1} \nСумма цифр от 1 до {S} равна {c2} \nМаксимальное четное число меньше N равно {c3} " );
         Console.ReadLine();
     }
 }
